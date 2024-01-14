6f0a3e8b6d477aa1163a41a0dc6374f69a53ca74
Vitya2007 <pimaksk@gmail.com>
2024-01-14 09:45:01 +0300

    added solution JavaSyntax task0214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string1 = sc.nextLine();
+        String string2 = sc.nextLine();
+        String string3 = sc.nextLine();
+        System.out.println(string3);
+        System.out.println(string2.toUpperCase());
+        System.out.println(string1.toLowerCase());
+
+
+
+    }
+}
