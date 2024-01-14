6c5433dacafe85c27cc2eca03d7b60f3db766248
Vitya2007 <pimaksk@gmail.com>
2024-01-14 11:18:23 +0300

    added solution JavaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "p";
+        String phrase = "f";
+        String line = "h";
+        String text = "j";
+
+    }
+}
