5cbbf5190228e7e838380c3b770aa54af5a7d629
Vitya2007 <pimaksk@gmail.com>
2024-01-15 23:40:37 +0300

    added solution JavaSyntax task0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int b = 0;
+        while (b<5)
+        {
+
+            int c = 0;
+            while (c<10)
+            {
+                System.out.print('Q');
+                c++;
+
+            }
+            System.out.println(" ");
+            b++;
+        }
+
+    }
+}
