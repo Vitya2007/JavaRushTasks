bb2bf03eb3ecf1722bf7e0d98d321294fbbfbdbf
Vitya2007 <pimaksk@gmail.com>
2024-01-15 21:13:06 +0300

    added solution JavaSyntax task0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int age = 0;
+        while (age < 100) {
+            System.out.println(quote);
+            age++;
+        }
+
+    }
+}
