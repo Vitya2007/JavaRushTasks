773cb1ec73a2a6d20176fbb7af671cbeb22de7c7
Vitya2007 <pimaksk@gmail.com>
2024-01-20 10:12:16 +0300

    added solution JavaSyntax task04014

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -7,9 +7,10 @@ package com.javarush.task.pro.task04.task0413;
 public class Solution {
     public static void main(String[] args) {
         String eight = "8";
-        for (int i = 0; i < 10; i++) {
+        for(int a = 0; a<10;a++) {
             System.out.println(eight);
             eight = eight.concat("8");
         }
+
     }
 }
