0ea5cee21fe1581cf4dd65c3702882f4832e3dbf
Vitya2007 <pimaksk@gmail.com>
2024-01-20 09:03:24 +0300

    added solution JavaSyntax task04016

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        double d = number1*1./number2;
+        System.out.println(d);
+    }
+}
